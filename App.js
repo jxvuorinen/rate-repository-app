5685b1f6897f32351d1fd93d68711c44f844ca78
Johanna Vuorinen <johanna.vuorinen@helsinki.fi>
2021-09-06 18:21:30 +0300

    first exercises

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('logi-viesti');
   return (
     <View style={styles.container}>
       <Text>App started!!</Text>
